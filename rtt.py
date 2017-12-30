345ad4a76c9635b7c5855938e11f2988b0debc0e
vicowara <mm.souichi@gmail.com>
2017-12-30 19:33:32 +0900

    refactoring

--- rtt.py
+++ rtt.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
+import subprocess
 import kivy
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, ListProperty
 from kivy.config import Config
-import subprocess
-import sys
 
 kivy.require("1.10.0")
 Config.set("graphics", "width", "120")
@@ -16,7 +16,7 @@ Config.set("graphics", "height", "90")
 
 class RootLayout(BoxLayout):
     text = StringProperty()
-    color = ListProperty([1, 1, 1, 1])
+    color = ListProperty([0, 0, 0, 0])
 
     def __init__(self, **kwargs):
         super(RootLayout, self).__init__(**kwargs)
@@ -28,7 +28,7 @@ class RootLayout(BoxLayout):
 
 def decide_color_level(rtt):
     max_thresholds = 100
-    rtt = rtt if max_thresholds > rtt else max_thresholds
+    rtt = rtt if rtt < max_thresholds else max_thresholds
     red = rtt / max_thresholds
 
     return [red, 0, 0, 0]
@@ -36,11 +36,11 @@ def decide_color_level(rtt):
 
 class RTTApp(App):
     def callback(self, dt):
-        cmd = ("ping 192.168.1.1 -c 1|"
-               "perl -anle 'print $1 if /bytes/ && $F[6] =~ /([0-9.]+)/'")
-
+        cmd = ("ping 192.168.1.1 -c 1"
+               "|perl -anle 'print $1 if /bytes/ && $F[6] =~ /([0-9.]+)/'")
         p = subprocess.Popen(cmd, shell=True,
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
         try:
             stdout, stderr = p.communicate(timeout=0.5)
         except subprocess.TimeoutExpired:
@@ -52,9 +52,7 @@ class RTTApp(App):
 
     def build(self):
         self.layout = RootLayout()
-        Clock.schedule_interval(
-            self.callback, 0.5
-        )
+        Clock.schedule_interval(self.callback, 0.5)
         return self.layout
 
 
